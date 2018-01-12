61cc697700ac3f5cb318357ba574fb0e1a32ca38
CGally <32678174+CGally@users.noreply.github.com>
2018-01-12 15:47:35 -0500

    Update collaborators_controller.rb

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -28,6 +28,6 @@ class CollaboratorsController < ApplicationController
   private
 
   def set_wiki
-    @wiki = Wiki.find(params[:wiki_id])
+    @wiki = Wiki.friendly.find(params[:wiki_id])
   end
 end
